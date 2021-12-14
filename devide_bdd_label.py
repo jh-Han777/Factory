205c40ec6a8d187b9cbf5274f0b328a3f2656fd9
jh-Han777 <78775224+jh-Han777@users.noreply.github.com>
2021-12-14 15:27:43 +0900

    Update devide_bdd_label.py

--- devide_bdd_label.py
+++ devide_bdd_label.py
@@ -2,8 +2,11 @@ import json
 
 with open('/media/sda1/paper3/data/bdd100k/labels/bdd100k_labels_images_train.json','r') as f:
     labels_ = json.load(f)
-
+##night
 labels = [label['name']+'\n'for label in labels_ if label['attributes']['timeofday'] == 'night']
 
-with open("/media/sda1/paper3/data/bdd100k/ImageSets/train_daytime.txt",'w') as f:
+##daytime
+#labels = [label['name']+'\n'for label in labels_ if label['attributes']['timeofday'] == 'night']
+
+with open("/media/sda1/paper3/data/bdd100k/ImageSets/train_night.txt",'w') as f:
     f.writelines(labels)
