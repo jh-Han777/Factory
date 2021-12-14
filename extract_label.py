1becc8a75debbd6206c169e377a256fe6553c724
jh-Han777 <78775224+jh-Han777@users.noreply.github.com>
2021-12-14 14:04:59 +0900

    Update extract_label.py

--- extract_label.py
+++ extract_label.py
@@ -3,7 +3,7 @@ import json
 with open('/media/sda1/paper3/data/bdd100k/labels/bdd100k_labels_images_train.json','r') as f:
     labels_ = json.load(f)
 
-labels = [label['name']+'\n'for label in labels_ if label['attributes']['timeofday'] != 'night']
+labels = [label['name']+'\n'for label in labels_ if label['attributes']['timeofday'] == 'night']
 
 with open("/media/sda1/paper3/data/bdd100k/ImageSets/train_daytime.txt",'w') as f:
     f.writelines(labels)
